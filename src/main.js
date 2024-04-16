67ff01c81a0ea357be831ec2e23a0842a29194c5
Cassiano Calian <130513232+CassianoCalian@users.noreply.github.com>
2024-04-15 22:22:09 -0300

    insert the header

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll("[data-tab-button]");
   const questions = document.querySelectorAll("[data-faq-question");
 
+  const heroSection = document.querySelector(".hero");
+  const heroEight = heroSection.clientHeight;
+
+  window.addEventListener("scroll", function () {
+    const actualPosition = window.scrollY;
+
+    if (actualPosition < heroEight) {
+      hiddenElementsOfHeader();
+    } else {
+      showHeaderElements();
+    }
+  });
+
+  //Seção de atrações, programação das abas
+
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (botao) {
       const abaAlvo = botao.target.dataset.tabButton;
@@ -13,10 +28,22 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  // Seção FAQ, accordion
+
   for (let i = 0; i < questions.length; i++) {
     questions[i].addEventListener("click", openOrCloseAnswer);
   }
 });
+
+function hiddenElementsOfHeader() {
+  const header = document.querySelector("header");
+  header.classList.add("header--is-hidden");
+}
+
+function showHeaderElements() {
+  const header = document.querySelector("header");
+  header.classList.remove("header--is-hidden");
+}
 
 function openOrCloseAnswer(elemento) {
   const classe = "faq__questions__item--is-open";
